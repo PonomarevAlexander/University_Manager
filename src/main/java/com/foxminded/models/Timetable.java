5012fae82515ffe817bf8c67cfe5b445985342cc
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2021-03-17 13:28:48 +0800

    added id field

--- src/main/java/com/foxminded/models/Timetable.java
+++ src/main/java/com/foxminded/models/Timetable.java
@@ -4,16 +4,25 @@ import java.util.List;
 
 public class Timetable {
 
+	private int id;
     private List<Lesson> schedule;
 
     public Timetable(List<Lesson> schedule) {
         this.schedule = schedule;
     }
 
+    public int getId() {
+    	return id;
+    }
+    
     public List<Lesson> getSchedule() {
         return schedule;
     }
 
+    public void setId(int id) {
+    	this.id = id;
+    }
+    
     public void setSchedule(List<Lesson> schedule) {
         this.schedule = schedule;
     }
