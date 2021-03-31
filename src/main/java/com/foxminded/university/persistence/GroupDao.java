074c55d2f72967100c347b1c101800446d8bb98b
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2021-03-31 23:32:43 +0300

    added keyHolder

--- src/main/java/com/foxminded/university/persistence/GroupDao.java
+++ src/main/java/com/foxminded/university/persistence/GroupDao.java
@@ -1,12 +1,17 @@
 package com.foxminded.university.persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
-
 import com.foxminded.university.domain.models.Group;
 
 @Repository
@@ -17,7 +22,7 @@ public class GroupDao implements Dao<Group> {
     private static final String QUERY_INSERT = "insert into groups(name) values(?)";
     private static final String QUERY_SELECT_BY_ID = "select * from groups where id=?";
     private static final String QUERY_SELECT_ALL = "select * from groups";
-    private static final String QUERY_SELECT_GROUP_BY_STUDENT = "select g.* from groups g left join students s on s.group_id=g.id where s.id=?";
+    private static final String QUERY_SELECT_GROUP_BY_STUDENT = "select g.* from groups g left join students_groups sg on g.id=sg.group_id where sg.student_id=?";
     private static final String QUERY_UPDATE = "update groups set name=? where id=?";
     private static final String QUERY_DELETE = "delete from groups where id=?";
     private static final String COLUMN_ID = "id";
@@ -29,8 +34,10 @@ public class GroupDao implements Dao<Group> {
     }
 
     @Override
-    public void add(Group group) {
-        jdbcTemplate.update(QUERY_INSERT, group.getName());
+    public int add(Group group) {
+        GeneratedKeyHolder holder = new GeneratedKeyHolder();
+        jdbcTemplate.update(getInsertParametredStatement(group), holder);
+        return (int) holder.getKey().longValue();
     }
 
     @Override
@@ -63,6 +70,17 @@ public class GroupDao implements Dao<Group> {
             group.setId(resultSet.getInt(COLUMN_ID));
             group.setName(resultSet.getString(COLUMN_NAME));
             return group;
+        };
+    }
+    
+    private PreparedStatementCreator getInsertParametredStatement(Group group) {
+        return new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement statement = con.prepareStatement(QUERY_INSERT, Statement.RETURN_GENERATED_KEYS);
+                statement.setString(1, group.getName());
+                return statement;
+            }
         };
     }
 
--- src/main/java/com/foxminded/university/persistence/StudentDao.java
+++ src/main/java/com/foxminded/university/persistence/StudentDao.java
@@ -1,10 +1,16 @@
 package com.foxminded.university.persistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.foxminded.university.domain.models.Student;
@@ -15,11 +21,12 @@ public class StudentDao implements Dao<Student> {
     private JdbcTemplate jdbcTemplate;
 
     private static final String QUERY_INSERT = "INSERT INTO students(name, last_name, age) VALUES(?, ?, ?)";
+    private static final String QUERY_INSERT_GROUP_ID = "insert into students_groups(student_id, group_id) values(?,?)";
     private static final String QUERY_SELECT_BY_ID = "select * from students where id=?";
     private static final String QUERY_SELECT_ALL = "Select * from students";
-    private static final String QUERY_SELECT_STUDENTS_RELATED_GROUP = "select st.* from students st left join groups gr on gr.id=st.group_id where st.group_id=?";
+    private static final String QUERY_SELECT_STUDENTS_RELATED_GROUP = "select s.* from students s left join students_groups sg on s.id=sg.student_id where sg.group_id=?";
     private static final String QUERY_UPDATE = "update students set name=?, last_name=?, age=? where id=?";
-    private static final String QUERY_UPDATE_GROUP_ID = "update students set group_id=? where id=?";
+    private static final String QUERY_UPDATE_STUDENT_GROUP = "update students_groups set group_id=? where student_id=?";
     private static final String QUERY_DELETE_BY_ID = "delete from students where id=?";
     private static final String COLUMN_NAME = "name";
     private static final String COLUMN_LAST_NAME = "last_name";
@@ -32,8 +39,10 @@ public class StudentDao implements Dao<Student> {
     }
 
     @Override
-    public void add(Student entity) {
-        jdbcTemplate.update(QUERY_INSERT, entity.getName(), entity.getLastName(), entity.getAge());
+    public int add(Student student) {
+        GeneratedKeyHolder holder = new GeneratedKeyHolder();
+        jdbcTemplate.update(getInsertParametredStatement(student), holder);
+        return (int) holder.getKey().longValue();
     }
 
     @Override
@@ -60,10 +69,14 @@ public class StudentDao implements Dao<Student> {
         return jdbcTemplate.query(QUERY_SELECT_STUDENTS_RELATED_GROUP, getRowMapper(), groupId);
     }
 
-    public void setGroupToStudent(int studentId, int groupId) {
-        jdbcTemplate.update(QUERY_UPDATE_GROUP_ID, groupId, studentId);
+    public void assignStudentToGroup(int studentId, int groupId) {
+        jdbcTemplate.update(QUERY_INSERT_GROUP_ID, studentId, groupId);
     }
     
+    public void updateStudentRelatedGroup(int studentId, int groupId) {
+        jdbcTemplate.update(QUERY_UPDATE_STUDENT_GROUP, groupId, studentId);
+    }
+
     private RowMapper<Student> getRowMapper() {
         return (resultSet, rowNum) -> {
             Student student = new Student();
@@ -75,4 +88,16 @@ public class StudentDao implements Dao<Student> {
         };
     }
 
+    private PreparedStatementCreator getInsertParametredStatement(Student student) {
+        return new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+                PreparedStatement statement = con.prepareStatement(QUERY_INSERT, Statement.RETURN_GENERATED_KEYS);
+                statement.setString(1, student.getName());
+                statement.setString(2, student.getLastName());
+                statement.setInt(3, student.getAge());
+                return statement;
+            }
+        };
+    }
 }
