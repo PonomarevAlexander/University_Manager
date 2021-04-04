89c467cd858dc902439bc825416418e82f8b8f37
Alexander Ponomarev <ponomarev.alex.1992@gmail.com>
2021-04-05 01:19:24 +0300

    added lesson service whith tests

---
+++ src/main/java/com/foxminded/university/domain/services/LessonService.java
@@ -0,0 +1,70 @@
+package com.foxminded.university.domain.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.foxminded.university.domain.models.Lesson;
+import com.foxminded.university.persistence.GroupDao;
+import com.foxminded.university.persistence.LessonDao;
+import com.foxminded.university.persistence.TeacherDao;
+import com.foxminded.university.persistence.TimetableDao;
+
+public class LessonService implements Service<Lesson> {
+    
+    private GroupDao groupDao;
+    private LessonDao lessonDao;
+    private TeacherDao teacherDao;
+    
+    @Autowired
+    public void setTeacherDao(TeacherDao teacherDao) {
+        this.teacherDao = teacherDao;
+    }
+
+    @Autowired
+    public void setLessonDao(LessonDao lessonDao) {
+        this.lessonDao = lessonDao;
+    }
+    
+    @Autowired
+    public void setGroupDao(GroupDao groupDao) {
+        this.groupDao = groupDao;
+    }
+
+    @Override
+    public void add(Lesson lesson) {
+        lessonDao.add(lesson);
+    }
+
+    @Override
+    public Lesson getById(int id) {
+        Lesson lesson = lessonDao.get(id);
+        lesson.setTeacher(
+                teacherDao.getTeacherByLessonId(id));
+        lesson.setGroup(
+                groupDao.getGroupByLesson(id));
+        return lesson;
+    }
+
+    @Override
+    public List<Lesson> getAll() {
+        List<Lesson> lessonsList = lessonDao.getAll();
+        lessonsList.forEach(lesson -> {
+            lesson.setTeacher(
+                    teacherDao.getTeacherByLessonId(lesson.getId()));
+            lesson.setGroup(
+                    groupDao.getGroupByLesson(lesson.getId()));
+        });
+        return lessonsList;
+    }
+
+    @Override
+    public void update(Lesson lesson) {
+        lessonDao.update(lesson);
+    }
+
+    @Override
+    public void remove(int id) {
+        lessonDao.remove(id);
+    }
+}
